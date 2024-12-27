4a479f97a84602f56e006423a53db0401dcc736d
Syed Ali Sher <139209183+Alyiscoding@users.noreply.github.com>
2024-12-27 18:17:27 +0500

    Add files via upload

---
+++ 5: CRUD/src/EmployeData.js
@@ -0,0 +1,23 @@
+export const Data =[{
+    id: 1,
+    firstName:"Syed Ali",
+    lastName:"Ali",
+    age:21
+},
+{
+    id: 2,
+    firstName:"Jamshaid",
+    lastName:"Jamil",
+    age:22
+},
+{
+    id: 3,
+    firstName:"Umar",
+    lastName:"Azhar",
+    age:32
+}
+
+
+
+
+]
